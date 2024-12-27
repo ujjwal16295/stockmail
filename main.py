feebadd58ff7cba044cb5d0a96a907cda30b9c97
ujjwal16295 <115917075+ujjwal16295@users.noreply.github.com>
2024-12-27 22:00:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,10 +6,31 @@ from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
 
+# add firebase config here
+firebase_config={
+  "type": os.getenv("TYPE"),
+  "project_id": os.getenv("PROJECT_ID"),
+  "private_key_id": os.getenv("PRIVATE_KEY_ID"),
+  "private_key": os.getenv("PRIVATE_KEY"),
+  "client_email": os.getenv("CLIENT_EMAIL"),
+  "client_id": os.getenv("CLIENT_ID"),
+  "auth_uri":  os.getenv("AUTH_URI"),
+  "token_uri": os.getenv("TOKEN_URI"),
+  "auth_provider_x509_cert_url": os.getenv("AUTH_PROVIDER_X509_CERT_URL"),
+  "client_x509_cert_url": os.getenv("CLIENT_X509_CERT_URL"),
+  "universe_domain": os.getenv("UNIVERSE_DOMAIN")
+}
 
-cred = credentials.Certificate("venv/serviceAccountKey.json")
+
+
+cred = credentials.Certificate(firebase_config)
 firebase_admin.initialize_app(cred)
 db=firestore.client()
+
+
+
+
+
 
 
 # Reference to your collection
@@ -110,9 +131,9 @@ print(rank)
 
 
 
-sender_email = "ujjwalpatel000777@gmail.com"
-receiver_email = "newujjwalpatel@gmail.com"
-password = "hdic suzn eesl qytc"
+sender_email = os.getenv("EMAIL")
+receiver_email = os.getenv("RECEIVER")
+password = os.getenv("PASSWORD")
 subject = "Stock Recommendation "
 body = ""
 
